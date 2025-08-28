cec5b61fba3ed2730b6ebd698e537882841907b4
Giustizia6174 <chu_zifan@126.com>
2025-08-28 21:43:40 +0800

    submit homework

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,13 @@
-# Add your commands here
+#!/bin/bash
+
+docker build -t hw_copy_image -f hw_copy/Dockerfile .
+
+echo "Testing addition:"
+docker run --rm hw_copy_image "/app/add \$0 \$1" 3 4
+
+echo -e "\nTesting multiplication:"
+docker run --rm hw_copy_image "/app/mul \$0 \$1" 3 4
+
+echo -e "\nUsage examples:"
+echo "docker run --rm hw_copy_image \"/app/mul \\\$0 \\\$1\" 5 6"
+echo "docker run --rm hw_copy_image \"/app/add \\\$0 \\\$1\" 5 6"
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,19 @@
-# Add your commands here
+#!/bin/bash
+# 用法: ./run.sh <program> <num1> <num2>
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <program> <num1> <num2>"
+    echo "Example: $0 mul 3 4"
+    exit 1
+fi
+
+program=$1
+num1=$2
+num2=$3
+
+docker build -t hw_vol_image -f hw_vol/Dockerfile .
+
+echo "Compiling and running $program with arguments $num1 $num2:"
+docker run --rm \
+  --mount type=bind,source=$(pwd)/src,target=/app \
+  hw_vol_image \
+  sh -c "g++ -o /app/$program /app/${program}.cpp && /app/$program $num1 $num2"
